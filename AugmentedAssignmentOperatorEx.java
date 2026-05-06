e8c45d7846b9c7835c1090baa70508acc8082091
Mrunali Dalal <76763340+MrunaliDalal25@users.noreply.github.com>
2026-05-06 07:56:43 +0530

    Add files via upload

---
+++ AugmentedAssignmentOperatorEx.java
@@ -0,0 +1,17 @@
+public class AugmentedAssignmentOperatorEx {
+    public static void main(String[] args){
+        int x=20;
+        int y=5;
+        //augemented assignment Operation
+        //basic way to write
+        //x=x+y;
+
+        //x+=y;
+        //x-=y;
+       // x*=y;
+       // x/=y;
+         x%=y;
+
+        System.out.println(x);
+    }
+}
